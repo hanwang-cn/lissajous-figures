02ae90372c3964283c34cb98fbb469cdde0659b9
Han Wang <60735780+hanwang-cn@users.noreply.github.com>
2020-10-12 15:44:47 +0800

    Update gui-new.py

--- gui-new.py
+++ gui-new.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from Lissajous_Figures import *
 
-def Get_and_Make():
+'''def Get_and_Make():
     global E1
     global E2
     global E3
@@ -12,7 +12,7 @@ def Get_and_Make():
     a2 = E4.get()
     num2 = E3.get()
     Chr = E5.get()
-    return make1(a1, num1,a2, num2, Chr)
+    return make1(a1, num1,a2, num2, Chr)'''
 
 
 root = Tk()
@@ -53,7 +53,6 @@ label5.pack(side=LEFT)
 E4 = Entry(frame5, bd = 2)
 E4.pack(side = LEFT)
 
-
 label6 = Label(frame6,text="Give a Name of this Gif(eg. hello.gif)",font = 'consolas', justify=LEFT)
 label6.pack(side=LEFT)
 E5 = Entry(frame6, bd = 2)
@@ -66,22 +65,13 @@ E5.pack(side = LEFT)
     # num2 = float(chr3)
 #    Chr = chr5
 #    return make(a1, num1,a2, num2, Chr)
-#def get(chr):
- #   global Entry
-    # chr=Entry.get()
-    # entry.get()
-    # 其次，您可以将其绑定到tkinter.Variable（它使用自动生成的名称创建和包装Tcl全局变量）。通常，使用它的子类StringVar-
-    # 它str在获取/设置时将值转换为。
-    # v = tkinter.StringVar()
-    # entry = tk.Entry(root1, textvariable=v)
-    # return chr
-    # global Entry
-   # v = StringVar()
- #   entry = Entry(root, textvariable = v)
-  #  # chr = StringVar()
- #   chr = v.get()
- #   return chr'''
-
+def Get_and_Make():
+    a1 = E2.get()
+    num1 = E1.get()
+    a2 = E4.get()
+    num2 = E3.get()
+    Chr = E5.get()
+    return make1(a1, num1,a2, num2, Chr)
 
 make_the_gif = Button(frame7, text="Make!", font = 'consolas', command=Get_and_Make())
 make_the_gif.pack(side=LEFT)
